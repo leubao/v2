8d53075fe66536ca2ea076dd88d31eb99d3cbde1
IT Work <zhoujing@chengde360.com>
2019-02-13 18:58:15 +0800

    改进提现管理计划任务

--- lub/Libs/Service/AutoCash.class.php
+++ lub/Libs/Service/AutoCash.class.php
@@ -24,37 +24,85 @@ class AutoCash extends \Libs\System\Service {
             //构造提现数据
             foreach ($list as $key => $v) {
                 if(AutoCash::check_cash($v['id'],$v['cash'],$datetime) == '200'){
-                    $postData[] = array(
+                    //主包日志
+                    $mainData[] = [
                         'sn'        => get_order_sn('6'),
-                        'user_id'   => $v['id'],
-                        'openid'    => AutoCash::get_openid($v['id']),
+                        'user_id'   => $data['id'],
                         'datetime'  => $datetime,
                         'createtime'=> time(),
                         'uptime'    => time(),
-                        'money'     => $v['cash'],
+                        'money'     => $data['cash'],
                         'remark'    => '系统自动创建',
                         'pay_type'  => '5',
-                        'status'    => '3',//待审核
-                    );
+                        'status'    => '6',//分包中
+                    ];
                 }
             }
-            //查询该客户是否存在原有未处理的提现申请。存在即作废
-            //批量写入提现数据
-            if(!empty($postData)){
-                if(M('Cash')->addAll($postData)){
+            //批量写入提现数据$mainData
+            if(!empty($mainData)){
+                $cash = D('cash')->addAll($mainData);
+                if($cash){
                     //重置用户余额
-                    foreach ($postData as $k => $va) {
+                    foreach ($mainData as $k => $va) {
                         AutoCash::up_user_cash($va['user_id'],$va['money']);
                     }
-                    return '200';
+                    $red = D('cash')->where(['status'=>6])->field('id,sn,user_id,money')->select();
+                    foreach ($red as $ka => $ve) {
+                       //判断是否需要分包
+                        $pack = AutoCash::create_pack($ve);
+                        $redData = empty($redData) ? $pack : array_merge($redData,$pack); 
+                    }
+                    $red = $model->table(C('DB_PREFIX').'cash_log')->addAll($redData);
+                    if($red){
+                        return '200';
+                    }else{
+                        return false;
+                    }
                 }else{
-                    error_insert("400078");
                     return false;
                 }
             }else{
                 return true;
             }
         }
+    }
+    //构建返利包
+    public function create_pack($data)
+    {   
+        $num = 0;
+        if($data['cash'] > 495){
+            $num = (int)floor($data['cash']/495);
+            $mod = $this->Kmod($data['cash'],495);
+            $redArr[] = array_merge([$mod],str_split(str_repeat(495, $num),3));
+            foreach ($redArr as $k => $v) {
+               $return[] = AutoCash::create_data($data, $v, $k);
+            }
+        }else{
+            $return[] = AutoCash::create_data($data); 
+        }
+        return $return;
+    }
+    //取余
+    function Kmod($bn, $sn)
+    {
+        //fmod() 函数返回除法的浮点数余数
+        return intval(fmod(floatval($bn), $sn));
+    }
+    static function create_data($data, $cash = 0, $k)
+    {
+        $datetime = date('Y-m-d H:i:s');
+        $return = array(
+            'no'        => $data['sn'].$k,
+            'cash_id'   => $data['id'],
+            'wx_no'     => '0',
+            'open_id'   => AutoCash::get_openid($data['user_id']),
+            'issuetime' => $datetime,
+            'updatetime'=> $datetime,
+            'money'     => $cash > 0 ? $cash : $data['money'],
+            'remark'    => '系统自动创建',
+            'status'    => '3',//待审核
+        );
+        return $return;
     }
     /**
      * 查询是否存在未处理的提现申请
@@ -67,7 +115,6 @@ class AutoCash extends \Libs\System\Service {
         if(empty($count)){
             return '200';
         }else{
-           // return AutoCash::invalid($userid,$count,$cash);
            return '400';
         }
     }
@@ -82,24 +129,6 @@ class AutoCash extends \Libs\System\Service {
         }
         $openid = M('WxMember')->where(array('user_id'=>$id))->getField('openid');
         return $openid;
-    }
-    /**
-     * 更新操作
-     * @param $string 要作废的数据集ID
-     */
-    function invalid($userid,$cash_id,$cash){
-        $map['id'] = $cash_id;
-        $map['status'] = '3';
-        $status = M('Cash')->where($map)->save(array('money'=>array('exp','money+'.$cash),'createtime'=>time()));
-        if($status){
-            AutoCash::up_user_cash($userid,$cash);
-            return '300';
-        }else{
-            //记录错误集合
-            //且跳过
-            error_insert('409'.$id);
-            return '300';
-        }
     }
     /**
      * 更新客户可用余额
